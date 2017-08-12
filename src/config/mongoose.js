ecf275f764f2e64e686f75c1814936dc2421158a
Diego Stratta <strattadb@gmail.com>
2017-08-11 23:47:36 -0300

    Update mongoose.js

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -6,13 +6,13 @@
 import logger from 'winston';
 
 export default async function configureMongo(mongoose, uri) {
-  mongoose.connect(uri, { useMongoClient: true });
-  const connection = mongoose.connection;
-
+  const { connection } = mongoose;
   connection
     .on('error', err => logger.error(err))
     .on('open', () => logger.info(`MongoDB connection established at ${uri}`))
-    .on('disconnected', () => logger.info('MongoDB disconnected'));
+    .on('disconnected', () => logger.info('MongoDB connection closed'));
+
+  await mongoose.connect(uri, { useMongoClient: true });
 
   // Close the MongoDB connection if server is killed
   process.on('SIGINT', () => connection.close(() => process.exit(0)));
